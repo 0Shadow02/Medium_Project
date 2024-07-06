57c439e05d3400c6295f04b6bea46041f9d36209
Shadow <dark35854@gmail.com>
2024-07-06 18:37:38 +0530

    signup_route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,54 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup',async (c)=> {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+    const body = await c.req.json()
+  try {
+    await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password,
+      name: body.name
+    }
+    
+  })
+  return c.text("User created successfully!")
+  } catch (error:any) {
+    return c.text(error)
+  }
+  
+
+
+   return c.text("Signup route")
+})
+app.post('/api/v1/user/signin', (c:any)=> {
+  return c.text("Signin route")
+})
+
+app.post('/api/v1/blog' ,(c)=>{
+  return c.text("blog posted")
+})
+app.put('/api/v1/blog' ,(c)=>{
+  return c.text("edit blog")
+})
+app.get('/api/v1/blog/:id' ,(c)=>{
+  return c.text("blog with userid: something")
+})
+app.get('/api/v1/blog/bulk/all' ,(c)=>{
+  return c.text("all blogs presented")
 })
 
 export default app
+
+
+
