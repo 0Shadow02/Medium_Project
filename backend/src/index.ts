74d8e9160d391951514ee2f21ba226bcd4486c0e
Shadow <dark35854@gmail.com>
2024-07-06 19:50:38 +0530

    jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
-
-import { PrismaClient } from '@prisma/client/edge'
+import { decode, jwt, sign, verify } from 'hono/jwt'
+import { Prisma, PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+const secret = 'mySecretKey'
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string
+    JWT_SECRET : string
 	}
 }>()
 
@@ -13,26 +15,64 @@ app.post('/api/v1/user/signup',async (c)=> {
     datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
     const body = await c.req.json()
-  try {
-    await prisma.user.create({
+    const response = await prisma.user.findFirst({
+      where:{
+        email: body.email,
+      }
+    })
+    if (response) {
+      return c.text("user already exist!")
+    } else {
+        try {
+    const User= await prisma.user.create({
     data:{
       email: body.email,
       password: body.password,
       name: body.name
     }
     
-  })
-  return c.text("User created successfully!")
+    })
+  const payload = {
+    id : User.id
+  }
+  const token = await sign(payload,c.env.JWT_SECRET)
+  return c.json({msg:"User created successfully!" , token:"Bearer "+ token})
   } catch (error:any) {
     return c.text(error)
   }
-  
+    }
 
-
-   return c.text("Signup route")
+   
 })
-app.post('/api/v1/user/signin', (c:any)=> {
-  return c.text("Signin route")
+app.post('/api/v1/user/signin',async (c:any)=> {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+  const body = await c.req.json()
+  try {
+    const User = await prisma.user.findFirst({
+      where:{
+        email: body.email,
+      }
+    })
+    if (User) {
+      const payload = {
+        id : User.id
+      }
+      const token = await sign(payload,c.env.JWT_SECRET)
+      
+      return c.json({msg:"user logined successfully!",toker:"Bearer "+ token})
+    } else {
+      return c.text(
+        "User doesn't exist!"
+      )
+    }
+
+  } catch (error:any) {
+    return c.text(error)
+  }
+
+  
 })
 
 app.post('/api/v1/blog' ,(c)=>{
