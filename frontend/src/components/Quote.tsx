b35e97e006cd8c97240e43472a6440169d42b9c9
Shadow <dark35854@gmail.com>
2024-07-15 19:30:06 +0530

    .env

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -7,9 +7,7 @@ export const Quote = () => {
             "The customer support I received was exceptional . The support team
             went above and beyond to address my concerns"
           </div>
-          <div className="max-w-md text-xl font-medium mt-4">
-            Julies Winfield
-          </div>
+          <div className="max-w-md text-xl font-medium mt-4">Shadow</div>
           <div className="max-w-md  font-light text-sm  text-slate-500">
             CEO | Acme corp
           </div>
